b89536f9430db2097bdac3e0579f6a4f1ecf72a7
ClementPagran <clementitou31@gmail.com>
2021-11-29 20:54:50 +0100

    TP2

--- SIS.m
+++ SIS.m
@@ -1,20 +1,25 @@
-function [xk,wk] = simulationDonnees(x,w,z,N,N_nbr,m,P,Qw,F,Rv,HFull);
-for k=1:N
-    X=array(N,N_nbr)
-    W=array(N,N_nbr)
-    if k==1
-        Z=randn(N,1);
-        L=sqrt (chol(P))';
-        X(1,:)=m+L*Z;
+function [X,W] = SIS(x,N,N_nbr,m,P,Qw,F,Rv,HFull);
+    X=zeros(size(x));
+    W=zeros(1,N_nbr);
+for k=0:size(x,2)
+    if k==0
+        y=randn(6,N);
+        L=(chol(P))';
+        for j=1:N
+            X(:,j)=m + L * y(:,j);
+        end
         W(1,:)=1/N;
     end
-    if k>=2
+    if k>=1
         for i=2:N_nbr
-            X(k+1,i)=
-            num1=exp((-1/2)*(x(k+1,:) - F(k,:))' / (Qw(k,k)) * (x(k+2,:) - F(k,:)));
-            num2=exp((-1/2)*(z(:,k) - HFull(:,k))' / (Rv(k,k)) * (z(:,k) - HFull(:,k)));
-
+            X(:,1)= Qw * randn(size(x,1),1) + F * x(:,1); %il faut rajouter le temps
+            pxkxkm1=exp((-1/2)*(X(:,i) - F*x(k,:))' / (Qw(k,k)) * (X(:,i) - F*x(:,i)));
+            pzkxk=exp((-1/2)*(z(:,i) - HFull*X(:,i))' / (Rv * (z(:,i) - HFull*X(:,i))));
+            pxkxkm1zk= L * randn(size(x,1),1) + F *X(:,1);
+            W(1,i+1)=W(1,i)+(pxkxkm1*pzkxk)/pxkxkm1zk;
         end
+    sumPoids=sum(W(1,:));
+    W(:,k) = W(:,k)/sumPoids;
     end
 end
 end
--- main.m
+++ main.m
@@ -1,4 +1,7 @@
 
-plot_p=1;
+plot_p=0;
 [N,T,Z,F,Hfull,mX0,PX0,Qw,Rv,X] = simulationDonnees(plot_p);
+x=[];
+N_nbr=100;
+[xk,wk] = SIS(x,N,N_nbr,mX0,PX0,Qw,F,Rv,Hfull);
                                   
